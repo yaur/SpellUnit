65f07108ae38c81c665d486aeded7c544db84afc
yaur <dan.witt@gmail.com>
2014-03-15 12:46:00 -0400

    add extractor for string literals inexceptions

---
+++ SpellUnit.ExceptionLiteralsExtractor/ExceptionLiteralsExtractor.cs
@@ -0,0 +1,87 @@
+﻿using SpellUnit.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System.ComponentModel.Composition;
+
+namespace SpellUnit.ExceptionLiteralsExtractor
+{
+    [Export(typeof(IStringExtractor))]
+    public class ExceptionLiteralsExtractor : IStringExtractor
+    {
+        public IEnumerable<FragmentDescriptor> Extract(Type type)
+        {
+            // this is dumb but if cecil will let us get from BCL reflection primitives
+            // straight to their cecil equivilants it isn't well documented
+            AssemblyDefinition monoAssembly = AssemblyDefinition.ReadAssembly(type.Assembly.Location);
+            
+            var monoType = monoAssembly.Modules.SelectMany(x => x.GetTypes()).Where(x => x.FullName == type.FullName).Single();
+            var methods = monoType.Methods;
+            List<FragmentDescriptor> retv = new List<FragmentDescriptor>();
+            foreach (var method in methods)
+            {
+                var item = ValidateMethod(method);
+                retv.AddRange(item);
+            }
+            return retv;
+        }
+
+        private IEnumerable<FragmentDescriptor> ValidateMethod(MethodDefinition method)
+        {
+            var body = method.Body;
+            string arg = string.Empty;
+            var formatString = method.FullName + " exception string:{0}";
+            int count = 0;
+            foreach (var instr in body.Instructions)
+            {
+                if (instr.OpCode.Code == Code.Ldstr)
+                {
+                    // we have just loaded a string literal
+                    arg = (string)instr.Operand;
+                    continue;
+                }
+                if (instr.OpCode.Code == Code.Newobj)
+                {
+                    // we are newing up some object, we are only interested in
+                    // new objects if they derive from System.Exception
+                    var methRef = (Mono.Cecil.MethodReference)instr.Operand;
+                    var type = Type.GetType(methRef.DeclaringType.FullName);
+                    
+                    if (arg != String.Empty && typeof(System.Exception).IsAssignableFrom(type))
+                    {
+                        yield return new FragmentDescriptor
+                        {
+                            FragementType = FragmentType.Sentence,
+                            Name = string.Format(formatString, count++),
+                            Value = arg
+                        };
+                    }
+                    // at this point we have either:
+                    // returned our result
+                    // loaded a string and not newed an exception
+                    // not loaded a string
+                    // in any case we want to reset and continue
+                    arg = string.Empty;
+                }
+            }
+        }
+
+        public IEnumerable<FragmentDescriptor> Extract(System.Reflection.Assembly assembly)
+        {
+            // not DRY, but the alternative is to rescan the assembly on each type, blech
+            AssemblyDefinition monoAssembly = AssemblyDefinition.ReadAssembly(assembly.Location);
+            var methods = monoAssembly.Modules.SelectMany(x => x.GetTypes()).SelectMany(x=>x.Methods);
+            List<FragmentDescriptor> retv = new List<FragmentDescriptor>();
+            foreach (var method in methods)
+            {
+                var item = ValidateMethod(method);
+                if (item != null) retv.AddRange(item);
+            }
+            return retv;
+        }
+    }
+}
--- SpellUnitBasicTests/UsageDemo.cs
+++ SpellUnitBasicTests/UsageDemo.cs
@@ -21,6 +21,60 @@ using ServiceStack.ServiceHost;
 namespace SpellUnitBasicTests
 {
 
+    class BadExceptionClass
+    {
+        private static T DataAccessMethod<T>(T value) where T: new()
+        {
+            if (value == null)
+            {
+                throw new System.InvalidOperationException("I canr spell.");
+            }
+            return value;
+        }
+    }
+
+    class GoodExceptionClass
+    {
+        private static T DataAccessMethod<T>(T value) where T : new()
+        {
+            if (value == null)
+            {
+                throw new System.InvalidOperationException("I can spell.");
+            }
+            return value;
+        }
+    }
+
+    class BadExceptionEvenInClosuresClass
+    {
+        private static  int DataAccessMethod(int value)
+        {
+            int r = 0;
+            System.Threading.ManualResetEvent evt = new System.Threading.ManualResetEvent(false);
+            var workItem = System.Threading.ThreadPool.QueueUserWorkItem((_) =>
+                {
+                    if (r == 0)
+                    {
+                        throw new System.InvalidOperationException("I canr spell.");
+                    }
+                    r = value * value;
+                    evt.Set();
+                });
+            evt.WaitOne();
+            return r;
+        }
+    }
+
+    class BadExceptionInPropertyClass
+    {
+        public bool CanSpell
+        {
+            get
+            {
+                throw new System.InvalidOperationException("I canr spell.");
+            }
+        }
+    }
     public class BadlySpelledClass
     {
         [ApiMember(Description="This property is missspelled.")]
@@ -50,11 +104,12 @@ namespace SpellUnitBasicTests
     {
         public UsageDemo()
         {
+            // these are needed so that we don't outsmart MEF
             var force = new SpellUnit.ServiceStackExtractor.ServiceStackExtractor();
             var force2 = new NHunspellChecker.NHunspell();
+            var force3 = new SpellUnit.ExceptionLiteralsExtractor.ExceptionLiteralsExtractor();
         }
         [TestMethod]
-        
         public void SunnyDay()
         {
             var settings = SpellUnit.SpellUnitTestSettings.FromFile("DefaultSpellUnit.testsettings.xml");
@@ -109,12 +164,63 @@ namespace SpellUnitBasicTests
         public void IgnoredPropertyTest()
         {
             // in this test we have manually validate a property and added it to the ignore list
-            bool exceptionThrown = false;
+            // bool exceptionThrown = false; // will throw and die on exception
             var settings = SpellUnit.SpellUnitTestSettings.FromFile("IgnoreProperty.testsettings.xml");
             var runner = new SpellUnit.SpellUnitTest(settings);
             runner.TypeUnderTest = typeof(BadlySpelledClass);
             runner.Run();
             
+        }
+#if false
+        // this fails for now
+        [TestMethod]
+        public void BadlySpelledExceptionInClosure()
+        {
+            bool exceptionThrown = false;
+            var settings = SpellUnit.SpellUnitTestSettings.FromFile("DefaultSpellUnit.testsettings.xml");
+            var runner = new SpellUnit.SpellUnitTest(settings);
+            runner.TypeUnderTest = typeof(BadExceptionEvenInClosuresClass);
+
+            try
+            {
+                runner.Run();
+            }
+            catch (SpellUnit.SpellCheckException ex)
+            {
+                Assert.AreEqual("Value T SpellUnitBasicTests.BadExceptionClass::DataAccessMethod(T) exception string:0 failed rule NHunspell spell checker", ex.Message);
+                exceptionThrown = true;
+            }
+            Assert.AreEqual(true, exceptionThrown);
+        }
+#endif
+        [TestMethod]
+        public void SunnyDayException()
+        {
+            var settings = SpellUnit.SpellUnitTestSettings.FromFile("DefaultSpellUnit.testsettings.xml");
+            var runner = new SpellUnit.SpellUnitTest(settings);
+            runner.TypeUnderTest = typeof(GoodExceptionClass);
+
+            // shouldn't throw
+            runner.Run();
+        }
+        [TestMethod]
+        public void BadlySpelledException()
+        {
+            bool exceptionThrown = false;
+            var settings = SpellUnit.SpellUnitTestSettings.FromFile("DefaultSpellUnit.testsettings.xml");
+            var runner = new SpellUnit.SpellUnitTest(settings);
+            runner.TypeUnderTest = typeof(BadExceptionClass);
+
+            try
+            {
+                runner.Run();
+            }
+            catch (SpellUnit.SpellCheckException ex)
+            {
+                Assert.AreEqual("Value T SpellUnitBasicTests.BadExceptionClass::DataAccessMethod(T) exception string:0 failed rule NHunspell spell checker", ex.Message);
+                exceptionThrown = true;
+            }
+            Assert.AreEqual(true, exceptionThrown);
         }
 
     }
